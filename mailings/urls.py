0c4768957da221aeeb451814c21a67aaccdced4a
Krimsi13 <aleksejkrimsi@gmail.com>
2024-05-20 22:56:05 +0300

    CRUD_mailingsettings

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
 from mailings.apps import MailingsConfig
-# from mailings.views import ListView
+from mailings.views import MailingSettingsListView, MailingSettingsDetailView, MailingSettingsUpdateView, \
+    MailingSettingsDeleteView, MailingSettingsCreateView
 
 app_name = MailingsConfig.name
 
 urlpatterns = [
     # path('', ListView.as_view(), name='_list'),
+    path("settings/", MailingSettingsListView.as_view(), name="settings"),
+    path("settings/<int:pk>/", MailingSettingsDetailView.as_view(), name="settings_detail"),
+    path("settings/create/", MailingSettingsCreateView.as_view(), name="settings_create"),
+    path("settings/<int:pk>/update/", MailingSettingsUpdateView.as_view(), name="settings_update"),
+    path("settings/<int:pk>/delete/", MailingSettingsDeleteView.as_view(), name="settings_delete"),
 ]
--- mailings/views.py
+++ mailings/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 
-# Create your views here.
+from mailings.models import MailingSettings
+
+
+class MailingSettingsListView(ListView):
+    model = MailingSettings
+
+
+class MailingSettingsDetailView(DetailView):
+    model = MailingSettings
+
+
+class MailingSettingsCreateView(CreateView):
+    model = MailingSettings
+    fields = '__all__'
+    # form_class = MailingSettingsForm
+    success_url = reverse_lazy("mailings:settings")
+
+
+class MailingSettingsUpdateView(UpdateView):
+    model = MailingSettings
+    fields = '__all__'
+    success_url = reverse_lazy("mailings:settings")
+
+
+class MailingSettingsDeleteView(DeleteView):
+    model = MailingSettings
+    success_url = reverse_lazy("mailings:settings")
