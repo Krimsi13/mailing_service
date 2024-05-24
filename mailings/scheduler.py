30dc1a2d7b8ff82c4f08598e8fd159a2627992e9
Krimsi13 <aleksejkrimsi@gmail.com>
2024-05-24 21:12:31 +0300

    test commit comment

--- mailings/scheduler.py
+++ mailings/scheduler.py
@@ -1,9 +1,12 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from django_apscheduler.jobstores import DjangoJobStore
 
 from mailings.utils import send_mailing
 
 
 def start():
     scheduler = BackgroundScheduler()
+    scheduler.add_jobstore(DjangoJobStore(), "default")
+
     scheduler.add_job(send_mailing, 'interval', seconds=10)
     scheduler.start()
